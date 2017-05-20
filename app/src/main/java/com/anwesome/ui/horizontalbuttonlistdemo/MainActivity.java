a9261655d096815594984f20b51c5d35a1ebf391
Anwesh43 <anweshthecool0@gmail.com>
2017-05-20 17:24:12 +0530

    Using OnClickListener in app

--- app/src/main/java/com/anwesome/ui/horizontalbuttonlistdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/horizontalbuttonlistdemo/MainActivity.java
@@ -2,18 +2,26 @@ package com.anwesome.ui.horizontalbuttonlistdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.anwesome.ui.horizontalbuttonlist.HorizontalButtonList;
 import com.anwesome.ui.horizontalbuttonlist.HorizontalButtonView;
+import com.anwesome.ui.horizontalbuttonlist.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        HorizontalButtonView horizontalButtonView = new HorizontalButtonView(this);
+        HorizontalButtonList horizontalButtonList = new HorizontalButtonList(this);
         for(int i=0;i<7;i++) {
-            horizontalButtonView.addHorizontalButton();
+            final int index = i+1;
+            horizontalButtonList.addButton(new OnClickListener(){
+                public void onClick() {
+                    Toast.makeText(MainActivity.this, index+" clicked", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
-        setContentView(horizontalButtonView);
+        horizontalButtonList.show();
     }
 }
