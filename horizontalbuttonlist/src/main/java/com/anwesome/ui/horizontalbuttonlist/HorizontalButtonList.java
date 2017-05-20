4f7127937d9aaffba8be78d5ab5b7a5cbe99eefa
Anwesh43 <anweshthecool0@gmail.com>
2017-05-20 16:34:32 +0530

    Adding HorizontalButtonList

---
+++ horizontalbuttonlist/src/main/java/com/anwesome/ui/horizontalbuttonlist/HorizontalButtonList.java
@@ -0,0 +1,29 @@
+package com.anwesome.ui.horizontalbuttonlist;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+
+/**
+ * Created by anweshmishra on 20/05/17.
+ */
+public class HorizontalButtonList {
+    private Activity activity;
+    private HorizontalButtonView horizontalButtonView;
+    private boolean isShown = false;
+    public HorizontalButtonList(Activity activity) {
+        this.activity = activity;
+        horizontalButtonView = new HorizontalButtonView(activity);
+    }
+    public void addButton() {
+        if(!isShown) {
+            horizontalButtonView.addHorizontalButton();
+        }
+    }
+    public void show() {
+        if(!isShown) {
+            activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            activity.setContentView(horizontalButtonView);
+            isShown = true;
+        }
+    }
+}
