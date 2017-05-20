0b266bd85f11eace7470ca9f8479fee8c43cc82f
Anwesh43 <anweshthecool0@gmail.com>
2017-05-20 15:02:18 +0530

    Using HorizontalButtonView in app

--- app/src/main/java/com/anwesome/ui/horizontalbuttonlistdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/horizontalbuttonlistdemo/MainActivity.java
@@ -3,11 +3,17 @@ package com.anwesome.ui.horizontalbuttonlistdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.anwesome.ui.horizontalbuttonlist.HorizontalButtonView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        HorizontalButtonView horizontalButtonView = new HorizontalButtonView(this);
+        for(int i=0;i<7;i++) {
+            horizontalButtonView.addHorizontalButton();
+        }
+        setContentView(horizontalButtonView);
     }
 }
