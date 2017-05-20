4d275fe8c2897e295ae1d216d36a47eacf1e71ab
Anwesh43 <anweshthecool0@gmail.com>
2017-05-20 12:37:20 +0530

    Adding horizontal button

--- horizontalbuttonlist/src/main/java/com/anwesome/ui/horizontalbuttonlist/HorizontalButtonView.java
+++ horizontalbuttonlist/src/main/java/com/anwesome/ui/horizontalbuttonlist/HorizontalButtonView.java
@@ -2,7 +2,9 @@ package com.anwesome.ui.horizontalbuttonlist;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -20,5 +22,39 @@ public class HorizontalButtonView extends View {
     public boolean onTouchEvent(MotionEvent event) {
         return true;
     }
-
+    private class HorizontalButton {
+        private float x,y,w,h,scale = 0;
+        private int index;
+        public HorizontalButton(float x,float y,float w,float h) {
+            this.x = x;
+            this.y = y;
+            this.w = w;
+            this.h = h;
+        }
+        public void setIndex(int index) {
+            this.index = index;
+        }
+        public void draw(Canvas canvas,Paint paint) {
+            paint.setColor(Color.GRAY);
+            canvas.drawRoundRect(new RectF(x,y,x+w,y+h),w/10,w/10,paint);
+            paint.setColor(Color.GREEN);
+            canvas.save();
+            canvas.translate(x+w/2,y+h/2);
+            canvas.scale(scale,scale);
+            canvas.drawRoundRect(new RectF(-w/2,-h/2,w/2,h/2),w/10,w/10,paint);
+            canvas.restore();
+        }
+        public boolean handleTap(float x,float y) {
+            return x>=this.x && x<=this.x+w && y>=this.y && y<=this.y+h;
+        }
+        public void fill(float factor) {
+            scale = factor;
+        }
+        public void move(float factor) {
+            float targetX = w;
+            if(index %2 == 0) {
+                x = targetX*factor;
+            }
+        }
+    }
 }
